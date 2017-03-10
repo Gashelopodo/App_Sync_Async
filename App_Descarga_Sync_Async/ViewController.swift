b43933c1ffbae486806f28d25a7248931674a240
cice <cice@aula8PovMacN09.local>
2017-03-10 19:25:56 +0100

    Initial Commit

---
+++ App_Descarga_Sync_Async/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App_Descarga_Sync_Async
+//
+//  Created by cice on 10/3/17.
+//  Copyright © 2017 gashe.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
